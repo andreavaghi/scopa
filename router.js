d00561f53807db6d4be59e60d3ddb719ca5719d6
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2016-06-10 10:51:03 +0200

    add play view

--- router.js
+++ router.js
@@ -3,24 +3,31 @@ Router.configure({
 });
 
 Router.map(function() {
-  this.route('home', {
-    path: '/'
+  this.route('/', function() {
+    this.render('home')
   });
-  this.route('play', {
-    path: '/game/:_id',
-    data: function() {
-      var game = Games.findOne(this.params._id);
+  this.route('/game/:_id', function() {
+    this.render('play', {
+      data: function() {
+        return Games.findOne({
+          _id: this.params._id
+        });
 
-      game.player = game.players[Meteor.userId()];
-      game.yourTurn = game.currentTurn[0] === Meteor.userId();
+        var game = Games.findOne({
+          _id: this.params._id
+        });
 
-      var otherId = game.currentTurn[game.yourTurn ? 1 : 0];
-      game.otherPlayer = {
-        username: Meteor.users.findOne(otherId).username,
-        score: game.players[otherId].score
+        game.player = game.players[Meteor.userId()];
+        game.yourTurn = game.currentTurn[0] === Meteor.userId();
+
+        var otherId = game.currentTurn[game.yourTurn ? 1 : 0];
+        game.otherPlayer = {
+          username: Meteor.users.findOne(otherId).username,
+          score: game.players[otherId].score
+        }
+
+        return game;
       }
-
-      return game;
-    }
+    });
   })
 });
