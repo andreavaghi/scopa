06d384a0745f9cc1d1525a2e3f75637f0e4541b5
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2016-06-13 09:24:42 +0200

    move routes in lib folder

--- client/home.js
+++ client/home.js
@@ -3,10 +3,19 @@ function otherId(game) {
 }
 
 Template.gameList.helpers({
+  completedGames: function() {
+    return Games.find({ inProgress: false }).map(function(game) {
+      game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
+      game.finished = moment(game.finished).fromNow();
+
+      if (game.winner === 'tie') game.message = 'tied';
+      else if (game.winner === Meteor.userId()) game.message = 'won';
+      else game.message = 'lost';
+      return game;
+    });
+  },
   games: function() {
-    return Games.find({
-      inProgress: true
-    }).map(function(game) {
+    return Games.find({ inProgress: true }).map(function(game) {
       game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
       game.started = moment(game.started).fromNow();
       return game;
@@ -16,21 +25,14 @@ Template.gameList.helpers({
 
 Template.userList.helpers({
   users: function() {
-    var myId = Meteor.userId(),
-      cantPlayAgainst = [myId];
+    var myid = Meteor.userId(),
+      cantPlayAgainst = [myid];
 
-    Games.find({
-      inProgress: true
-    }).forEach(function(game) {
+    Games.find({ inProgress: true }).forEach(function(game) {
       cantPlayAgainst.push(otherId(game));
     });
-    return Meteor.users.find({
-      _id: {
-        $not: {
-          $in: cantPlayAgainst
-        }
-      }
-    });
+
+    return Meteor.users.find({ _id: { $not: { $in: cantPlayAgainst } } });
   }
 });
 
