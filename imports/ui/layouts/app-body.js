4f385a80c916bf390bd40ff7c32af4aac17e1631
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2016-07-01 08:00:56 +0200

    add logout

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,1 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
 import './app-body.html';
+
+Template.AppBody.events({
+  'click .js-logout' () {
+    Meteor.logout();
+  }
+});
